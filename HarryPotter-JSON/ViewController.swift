c40c6a551a6d64de0a692e13cc7dbd155df38041
Zaki <menza.zm@gmail.com>
2023-05-06 15:40:30 +0600

    Initial Commit

---
+++ HarryPotter-JSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotter-JSON
+//
+//  Created by Zaki on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
